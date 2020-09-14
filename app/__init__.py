ef96288c57adff1d8e8f821ed79643dd700b4de5
Daniel Gantz <daniel.gantz@gmail.com>
2020-09-14 17:21:08 -0400

    updates

--- app/__init__.py
+++ app/__init__.py
@@ -6,8 +6,10 @@ db = SQLAlchemy()
 
 def create_app(config_name):
     app = Flask(__name__)
-    app.config.from_object(config[config_name])
-    config[config_name].init_app(app)
+    env_config_class = config[config_name]
+
+    app.config.from_object(env_config_class)
+    env_config_class.init_app(app)
 
     db.init_app(app)
 
--- noshuff.py
+++ noshuff.py
@@ -3,8 +3,8 @@ from app import create_app, db
 from app.models import User
 
 app = create_app(os.getenv('FLASK_CONFIG') or 'default')
-migrate = Migrate(app, db)
+# migrate = Migrate(app, db)
 
 @app.shell_context_processor
 def make_shell_context():
-    return dict(User=User)
+    return dict(db=db, User=User)
