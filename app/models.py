dc55a0cd3123b9b7fb74309ec132a34578b704e3
Daniel Gantz <daniel.gantz@gmail.com>
2020-10-05 18:23:17 -0400

    Add posts model

--- app/models.py
+++ app/models.py
@@ -8,6 +8,9 @@ class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     spotify_id = db.Column(db.String(64), unique=True, index=True)
     email = db.Column(db.String(64), unique=True, index=True)
+    posts = db.relationship('Post', backref='user', lazy=True)
+
+#region
 
     # @staticmethod
     # def get_user_from_token(token):
@@ -31,3 +34,9 @@ class User(db.Model):
             current_app.config['JWT_SECRET'],
             algorithm='HS256'
         )
+#endregion
+class Post(db.Model):
+    __tablename__ = 'posts'
+    id = db.Column(db.Integer, primary_key=True)
+    spotify_playlist_id = db.Column(db.String(64))
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
