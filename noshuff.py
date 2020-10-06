a3b08e3f45beb55e360894a049c7d8a07ab83908
Daniel Gantz <daniel.gantz@gmail.com>
2020-10-06 13:19:38 -0400

    Custom module.filename cli test runner, and TDD request test

--- noshuff.py
+++ noshuff.py
@@ -1,17 +1,30 @@
 import os
 from app import create_app, db
-from app.models import User
+from app.models import User, Post
 from flask_migrate import Migrate
+import click
+import re
 
 app = create_app(os.getenv('FLASK_CONFIG'))
 migrate = Migrate(app, db)
 
 @app.shell_context_processor
 def make_shell_context():
-    return dict(db=db, User=User)
+    return dict(db=db, User=User, Post=Post)
 
 @app.cli.command()
-def test():
+@click.argument("target", required=False)
+def test(target=None):
     import unittest
-    tests = unittest.TestLoader().discover('tests')
+    if target:
+        module_search = re.compile(r"[.]\w+$").search(target)
+        if module_search:
+            module_name = target[:module_search.start()]
+            file_name = target[(module_search.start() + 1):module_search.end()]
+            tests = unittest.TestLoader().discover(f'tests.{module_name}', f'{file_name}*')
+        else:
+            tests = unittest.TestLoader().discover('tests', f'{target}*')
+    else:
+        tests = unittest.TestLoader().discover('tests')
+
     unittest.TextTestRunner(verbosity=2).run(tests)
---
+++ tests/test_requests/test_posts.py
@@ -0,0 +1,39 @@
+from .base import RequestTestBase
+from app import db
+from app.models import User, Post
+
+
+class TestUsers(RequestTestBase):
+    def setUp(self):
+        super(TestUsers, self).setUp()
+        self.headers = {
+            "Content-Type": "application/vnd.api+json",
+            "Accept": "application/vnd.api+json"
+        }
+        self.user = User(spotify_id='test_spotify_id')
+        db.session.add(self.user)
+        db.session.commit()
+
+    def test_users_get(self):
+        data = {
+            "data": {
+                "type": "posts",
+                "attributes": {
+                    "spotify_playlist_id": "test_spotify_playlist_id"
+                },
+                "relationships": {
+                    "user": {
+                        "data": {
+                            "type": "users",
+                            "id": str(self.user.id)
+                        }
+                    }
+                }
+            }
+        }
+
+        response = self.client.post('/api/v1/posts', json=data, headers=self.headers)
+
+        self.assertEqual(response.status_code, 201)
+        self.assertEqual(Post.query.count(), 1)
+        self.assertEqual(Post.query.first().user.spotify_id, self.user.spotify_id)
