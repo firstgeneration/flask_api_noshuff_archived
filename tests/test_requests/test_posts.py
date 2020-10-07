0b02016a1b02e9dd8c791989fe845cf7a25a5166
Daniel Gantz <daniel.gantz@gmail.com>
2020-10-07 16:45:35 -0400

    Update Post test to require auth, remove user from post data

--- tests/test_requests/test_posts.py
+++ tests/test_requests/test_posts.py
@@ -6,34 +6,42 @@ from app.models import User, Post
 class TestPosts(RequestTestBase):
     def setUp(self):
         super(TestPosts, self).setUp()
-        self.headers = {
-            "Content-Type": "application/vnd.api+json",
-            "Accept": "application/vnd.api+json"
-        }
         self.user = User(spotify_id='test_spotify_id')
         db.session.add(self.user)
         db.session.commit()
+        token = self.user.generate_access_token().decode('utf-8')
+        self.headers = {
+            "Content-Type": "application/vnd.api+json",
+            "Accept": "application/vnd.api+json",
+            'Authorization': f'Bearer {token}'
+        }
 
-    def test_users_get(self):
+    def test_post_post(self):
         data = {
             "data": {
                 "type": "posts",
                 "attributes": {
                     "spotify_playlist_id": "test_spotify_playlist_id"
-                },
-                "relationships": {
-                    "user": {
-                        "data": {
-                            "type": "users",
-                            "id": self.user.id
-                        }
-                    }
                 }
             }
         }
-
         response = self.client.post('/api/v1/posts', json=data, headers=self.headers)
 
         self.assertEqual(response.status_code, 201)
         self.assertEqual(Post.query.count(), 1)
         self.assertEqual(Post.query.first().user.spotify_id, self.user.spotify_id)
+
+    def test_post_post_wo_auth(self):
+        data = {
+            "data": {
+                "type": "posts",
+                "attributes": {
+                    "spotify_playlist_id": "test_spotify_playlist_id"
+                }
+            }
+        }
+        del self.headers['Authorization']
+        response = self.client.post('/api/v1/posts', json=data, headers=self.headers)
+
+        self.assertEqual(response.status_code, 401)
+        self.assertEqual(Post.query.count(), 0)
